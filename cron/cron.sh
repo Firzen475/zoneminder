3e3fc1add36ff1419f722724b335407ca91e3911
Firzen475 <firzen475@gmail.com>
2022-07-30 13:35:03 +0300

    Update cron.sh

--- cron/cron.sh
+++ cron/cron.sh
@@ -1,6 +1,10 @@
 
 datetime=$(date +"%Y-%m-%d")_$(date +"%I:%M:%S");
-mysqldump -u root zm > /backup/zm_$datetime.sql
-rm -rf /init/*.sql
+mysqldump -u root zm > /backup/zm_$datetime.sql;
+rm -rf /init/*.sql;
 cp /backup/zm_$datetime.sql /init/dump.sql;
-find /backup/*.sql -type f -mtime +7 -exec rm -rf {} \;
+find /backup/*.sql -type f -mtime +7 -exec rm -rf {} \;
+
+
+
+
